b053be5fa693d4ab72b4fca9fca81ca69331ce09
yogita296 <yogitakumari296@gmail.com>
2025-06-20 17:51:29 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,20 @@
+a=input('enter number one: ')
+b=input('enter number two: ')
+a=float(a)
+b=float(b)
+# Addition of two numbers
+A=a+b
+
+print('Addition of numbers is: ',A)
+
+# Subtraction of two numbers
+S=a-b
+print('Subtraction of numbers is: ',S)
+
+# Multiplication of two numbers
+M=a*b
+print('Multiplication of numbers is: ',M)
+
+# Division of two numbers
+D=a/b
+print('Division of numbers is: ',D)
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+a=input('enter first name: ')
+b=input('enter second name: ')
+print('Hello,'+a+' '+b +'! Welcome to the Python Program')
+
